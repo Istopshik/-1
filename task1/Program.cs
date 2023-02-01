a5bf9ded77ad9d73477a78eb812eb91edc185fde
Vitaliy Gunbin <vitalik_gunbin@mail.ru>
2023-02-01 16:31:02 +0300

    task 1 completed

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Введите первое число: ");
+string firstNumber = Console.ReadLine();
+int number1 = Convert.ToInt32(firstNumber);
+System.Console.WriteLine("Введите второе число");
+string secondNumber = Console.ReadLine();
+int number2 = Convert.ToInt32(secondNumber);
+if(number1 > number2)
+{
+    System.Console.WriteLine("Первое число больше, чем второе");
+}
+else if(number1 == number2)
+{
+    System.Console.WriteLine("Введеные вами числа равны");
+}
+else
+{
+    System.Console.WriteLine("Второе число больше, чем первое");
+}
