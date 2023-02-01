c37d76c5fe78365c4c72de7fe5b3a178894fae23
Vitaliy Gunbin <vitalik_gunbin@mail.ru>
2023-02-01 17:07:33 +0300

    task 3 Completed

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число и программа определит, является ли оно четным: ");
+string strNumber = Console.ReadLine();
+int number = Convert.ToInt32(strNumber);
+if(number % 2==0)
+{
+    System.Console.WriteLine("Число является четным");
+}
+else
+{
+    System.Console.WriteLine("Число ялвяется нечетным");
+}
