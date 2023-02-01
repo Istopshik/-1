de85fe4d2828fefd3e374d7e307c7e118cb75758
Vitaliy Gunbin <vitalik_gunbin@mail.ru>
2023-02-01 16:59:30 +0300

    Task 2 Completed

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Введите, первое число: ");
+string firstNumber = Console.ReadLine();
+System.Console.WriteLine("Введите, второе число: ");
+string secondNumber = Console.ReadLine();
+System.Console.WriteLine("Введите, третье число: ");
+string thirdNumber = Console.ReadLine();
+int number1 = Convert.ToInt32(firstNumber);
+int number2 = Convert.ToInt32(secondNumber);
+int number3 = Convert.ToInt32(thirdNumber);
+int max = number1;
+if(max >= number2);
+else
+{
+   max = number2;
+} 
+if(max >= number3);
+else
+{
+    max = number3;
+}
+System.Console.Write("Самое большое число из указанных: ");
+System.Console.Write(max);
