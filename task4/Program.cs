6ccbbc1819fcf8b028a802206eeaea7ed5c53d28
Vitaliy Gunbin <vitalik_gunbin@mail.ru>
2023-02-01 17:59:54 +0300

    4 задача не решена

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите число и программа покажет все четные числа находящиеся в диапозоне от 1 до вашего числа");
+string strNumber = Console.ReadLine();
+int number = Convert.ToInt32(strNumber);
+int i = 1;
+int result = N:;
+while (i <= number);
+{
+    if(i % 2 != 1)
+    {
+        System.Console.WriteLine(i + ", ");
+    }
+    i++;
+}
